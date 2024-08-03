50d26a06d8ec06baf04e81d46b35bae182f62586
rakpunar <r.akpunar@gmail.com>
2024-08-03 23:15:36 +0300

    Update app.js
    Added download feature in .srt format.

--- app.js
+++ app.js
@@ -3,52 +3,67 @@ let subtitleList = [];
 
 function msToTime(msParam) {
     let ms = Number(msParam);
-    let s = ((ms/1000)%60).toString();
-    let m = ((ms/(1000*60))%60).toString();
-    let h = ((ms/(1000*60*60))%24).toString();
-    ms = ms.toString()
-    if (Number(s) < 10){
-        s = "0"+s
+    let s = ((ms / 1000) % 60).toString();
+    let m = ((ms / (1000 * 60)) % 60).toString();
+    let h = ((ms / (1000 * 60 * 60)) % 24).toString();
+    ms = ms.toString();
+    if (Number(s) < 10) {
+        s = "0" + s;
     }
-    if (Number(m) < 10){
-        m = "0"+m
+    if (Number(m) < 10) {
+        m = "0" + m;
     }
-    if (Number(h) < 10){
-        h = "0"+h
+    if (Number(h) < 10) {
+        h = "0" + h;
     }
-    return h.substring(0,2)+":"+m.substring(0,2)+":"+s.substring(0,2)+","+ms.slice(ms.length - 3)
+    return h.substring(0, 2) + ":" + m.substring(0, 2) + ":" + s.substring(0, 2) + "," + ms.slice(ms.length - 3);
 }
 
 subtitleMenu[1].childNodes.forEach(e => {
-    if(e.className == "spacing"){
-    }else{
+    if (e.className == "spacing") {
+    } else {
         subtitleList.push(
-        {
-        "index": Number(e.childNodes[0].dataset["index"])+1,
-        "start": e.childNodes[0].dataset["start"],
-        "text": e.childNodes[0].innerText}
-        )
-    }});
+            {
+                "index": Number(e.childNodes[0].dataset["index"]) + 1,
+                "start": e.childNodes[0].dataset["start"],
+                "text": e.childNodes[0].innerText
+            }
+        );
+    }
+});
+
 let subtitleContent = "";
 for (let i = 0; i < subtitleList.length; i++) {
-    if(subtitleList[i+1] == undefined){
-        subtitleContent += subtitleList[i].index+"\n"+msToTime(subtitleList[i].start)+" --> "+msToTime(Number(subtitleList[i].start)+1000)+"\n"+subtitleList[i].text+"\n\n"
-    }else{
-        subtitleContent += subtitleList[i].index+"\n"+msToTime(subtitleList[i].start)+" --> "+msToTime(subtitleList[i+1].start)+"\n"+subtitleList[i].text+"\n\n"
+    if (subtitleList[i + 1] == undefined) {
+        subtitleContent += subtitleList[i].index + "\n" + msToTime(subtitleList[i].start) + " --> " + msToTime(Number(subtitleList[i].start) + 1000) + "\n" + subtitleList[i].text + "\n\n";
+    } else {
+        subtitleContent += subtitleList[i].index + "\n" + msToTime(subtitleList[i].start) + " --> " + msToTime(subtitleList[i + 1].start) + "\n" + subtitleList[i].text + "\n\n";
     }
 }
+
 let textFile = null,
-makeTextFile = function (text) {
-    let data = new Blob([text], {type: 'text/plain;charset=UTF-8'});
+    makeTextFile = function (text) {
+        let data = new Blob([text], { type: 'text/plain;charset=UTF-8' });
+        if (textFile !== null) {
+            window.URL.revokeObjectURL(textFile);
+        }
+        textFile = window.URL.createObjectURL(data);
+        return textFile;
+    };
 
-    // If we are replacing a previously generated file we need to
-    // manually revoke the object URL to avoid memory leaks.
-    if (textFile !== null) {
-        window.URL.revokeObjectURL(textFile);
-    }
+let downloadSRT = function () {
+    let link = document.createElement('a');
+    link.setAttribute('download', 'subtitles.srt');
+    link.href = makeTextFile(subtitleContent);
+    document.body.appendChild(link);
 
-    textFile = window.URL.createObjectURL(data);
-
-    return textFile;
+    // Wait for the link to be added to the document
+    window.requestAnimationFrame(function () {
+        let event = new MouseEvent('click');
+        link.dispatchEvent(event);
+        document.body.removeChild(link);
+    });
 };
-makeTextFile(subtitleContent)
+
+// .srt dosyasını oluştur ve indir
+downloadSRT();
